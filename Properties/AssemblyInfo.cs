4602ab1bf9aaf8ad6d375a69cae88caa3cdd165e
Dorian Muthig <contact@dorianmuthig.com>
2014-09-06 03:40:47 +0200

    fixed README Markdown syntax

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ZuneTune")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Microsoft IT 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ﻿ZuneTune
 ========
 
-#zunetune playlist generator
+\#zunetune playlist generator
 
 Please see http://www.insidethecircle.net/2009/06/presenting-zunetune-playlist-generator.html
+
+https://www.zunetune.net/
